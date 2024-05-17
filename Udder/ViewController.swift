263c1330b01e670169f01e493983fe8777fd2c79
Tomonori Tsukuda <tifory@yahoo.co.jp>
2024-05-17 16:19:35 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Tomonori Tsukuda on 2024/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
